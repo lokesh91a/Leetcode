302dde17bb26593bb14a2211e5ff2423c0c118e1
lokesh91a <lokesh91a@gmail.com>
2017-08-20 23:50:04 -0700

    test

--- Test.java
+++ Test.java
@@ -1,4 +1,5 @@
 
+
 import java.util.*;
 
 
@@ -15,6 +16,6 @@ public class Test {
                 c++;
             }
         }
-        System.out.println(c);
+        System.out.println(c++);
     }
 }
